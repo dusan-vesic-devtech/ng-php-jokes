f11eb12c5529d71a1afd796bfc3c454f1ed5f4a5
dusan-vesic-devtech <dusan.vesic@devtechgroup.com>
2017-05-19 15:06:21 +0200

    Isolate model

--- src/app/jokes-service.ts
+++ src/app/jokes-service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Joke } from './joke';
 
 @Injectable()
 export class JokesServiceService {
 
   constructor() { }
 
-  getJokes() {
+  getJokes(): Array<Joke> {
     return [
       {title: 'Joke 1'},
       {title: 'Joke 2'},
