5e1b343796020bbb5dd2b56322f769aee12cafe2
dusan-vesic-devtech <dusan.vesic@devtechgroup.com>
2017-05-19 16:08:54 +0200

    PHP server setup

--- server/getJokes.php
+++ server/getJokes.php
@@ -1,6 +1,12 @@
 <?php
 
-echo json_encode(
+header('Access-Control-Allow-Origin: http://localhost:4200');  //I have also tried the * wildcard and get the same response
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+
+echo json_encode([
     ['name' => 'alice'],
     ['name' => 'jane']
-);
+]);
--- src/app/jokes-form/jokes-form.component.ts
+++ src/app/jokes-form/jokes-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions } from "@angular/http";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'jokes-form',
@@ -35,14 +38,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JokesFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
   onJokeSubmit(title, content) {
+    this.http.get('https://jsonplaceholder.typicode.com/users/1')
+      .map(res => res.json())
+      // .subscribe(() => {
+      //   console.log('Ok');
+      // })
     console.log(title.value, content.value);
+    this.sendToServer(title.value, content.value)
+      .subscribe(res => console.log(res));
     title.value = '';
     content.value = '';
+  }
+
+  sendToServer(title, content) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post('http://localhost:8000/getJokes.php', { title, content }, options)
+                    // .map(res => res.text);
+                    // .do(() => { console.log('ok')})
+                    // .catch(this.handleError);
   }
 }
